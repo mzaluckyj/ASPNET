18c6744ebabd99376ba034ab3ddd4946d19fa217
Michael <m.zaluckyj@gmail.com>
2023-01-24 16:09:49 -0500

    exercise 1

---
+++ Testing/IProductsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductsRepository
+    {
+
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using System.Data;
+using Testing.Models;
+using System.Data;
+
+namespace Testing
+{
+    public class ProductRepository : IProductsRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+    }
+}
