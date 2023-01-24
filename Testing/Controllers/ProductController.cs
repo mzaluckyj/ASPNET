1a74a749e51c482378ac8dc0db07c7905e3a7a49
Michael <m.zaluckyj@gmail.com>
2023-01-24 16:17:55 -0500

    exercise2

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -24,5 +24,11 @@ namespace Testing.Controllers
             return View(products);
         }
 
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+
     }
 }
